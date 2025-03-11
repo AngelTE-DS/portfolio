096c145260386df2a894d7f32d53520435c7d0c2
AngelTE14 <angelte1400@gmail.com>
2025-03-11 12:20:37 -0500

    cambio

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "/Angel Terrones",
+  name: "/AngelTE",
   url: "https://angelterrones.vercel.app/",
   description: "Angel Terrones personal website.",
   author: "Angel Terrones Escobedo",
@@ -8,7 +8,7 @@ export const siteConfig = {
   links: {
     twitter: "https://twitter.com/ibai",
     github: "https://github.com/AngelTE14",
-    personalSite: "https://miulerbm.vercel.app/",
+    personalSite: "https://angelterrones.vercel.appp/",
     linkedin: "https://www.linkedin.com/in/angel-terrones-escobedo/",
   },
 };
